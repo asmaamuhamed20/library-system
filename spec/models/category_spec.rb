d72b3d5ef592595465f82c107224633964cda206
asmaa mohamed <asmaamuhamed71@gmail.com>
2024-08-08 00:31:55 +0300

    Updating tests files for books , users and categories

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:categorizations) }
+  it { should have_many(:books).through(:categorizations) }
+  it { should validate_presence_of(:name) }
+
+  describe 'Custom Behavior' do
+    let(:category) { Category.new(name: 'Science Fiction') }
+
+    it 'is valid with valid attributes' do
+      expect(category).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      category.name = nil
+      expect(category).not_to be_valid
+    end
+  end
 end
